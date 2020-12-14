b69246c0e433165976bf2225552ef4158a86a4d7
Máté Puskás <puskasmate2000@gmail.com>
2020-12-14 19:54:47 +0100

    Creating MovieController with finding and saving methods.

---
+++ src/main/java/com/example/crud/controller/MovieController.java
@@ -0,0 +1,37 @@
+package com.example.crud.controller;
+
+import com.example.crud.model.Movie;
+import com.example.crud.service.MovieService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class MovieController {
+
+    @Autowired
+    private MovieService movieService;
+
+
+    @GetMapping("/getMovieList")
+    public List<Movie> fetchMovieList() {
+        List<Movie> movies = new ArrayList<>();
+        movies = movieService.fetchMovieList();
+        return movies;
+    }
+
+    @PostMapping("/addMovieToList")
+    public Movie saveMovie(@RequestBody Movie movie) {
+        return movieService.saveMovieToDB(movie);
+    }
+
+    @GetMapping("/getMovieById/{id}")
+    public Movie fetchMovieById(@PathVariable int id) {
+
+
+        return null;
+    }
+
+}
