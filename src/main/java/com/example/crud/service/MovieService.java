db7dcf646cefd3b530389701414955520b6bd139
Máté Puskás <puskasmate2000@gmail.com>
2020-12-14 20:02:18 +0100

    Added the method of finding a movie by id.

--- src/main/java/com/example/crud/service/MovieService.java
+++ src/main/java/com/example/crud/service/MovieService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class MovieService {
@@ -20,6 +21,12 @@ public class MovieService {
     public Movie saveMovieToDB(Movie movie) {
 
         return movieRepo.save(movie);
+
+    }
+
+    public Optional<Movie> fetchMovieById(int movieId) {
+
+        return movieRepo.findById(movieId);
 
     }
 
