b04a0b3fb281d7eb6b1e8eb83b337712142f75b5
Máté Puskás <puskasmate2000@gmail.com>
2020-12-19 14:47:04 +0100

    Welcome message.

--- src/main/java/com/example/crud/CrudApplication.java
+++ src/main/java/com/example/crud/CrudApplication.java
@@ -2,12 +2,14 @@ package com.example.crud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class CrudApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrudApplication.class, args);
+		System.out.println("It is working!");
 	}
 
 }
