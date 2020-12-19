2801059afd10ea8be4fc285667a624c65cf0b8b8
Máté Puskás <puskasmate2000@gmail.com>
2020-12-19 14:42:05 +0100

    Added crossorigins to controller.

--- src/main/java/com/example/crud/controller/MovieController.java
+++ src/main/java/com/example/crud/controller/MovieController.java
@@ -1,10 +1,10 @@
 package com.example.crud.controller;
 
 import com.example.crud.model.Movie;
+import com.example.crud.repo.MovieRepo;
 import com.example.crud.service.MovieService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,9 +13,11 @@ public class MovieController {
 
     @Autowired
     private MovieService movieService;
+    private MovieRepo movieRepo;
 
 
     @GetMapping("/getMovieList")
+    @CrossOrigin(origins = "http://localhost:4200")
     public List<Movie> fetchMovieList() {
         List<Movie> movies = new ArrayList<>();
         movies = movieService.fetchMovieList();
@@ -23,18 +25,21 @@ public class MovieController {
     }
 
     @PostMapping("/addMovieToList")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Movie saveMovie(@RequestBody Movie movie) {
         return movieService.saveMovieToDB(movie);
     }
 
     @GetMapping("/getMovieById/{movieId}")
+    @CrossOrigin(origins = "http://localhost:4200")
     public Movie fetchMovieById(@PathVariable int movieId) {
 
         return movieService.fetchMovieById(movieId).get();
 
     }
 
-    @GetMapping("/deleteMovieById/{movieId}")
+    @DeleteMapping("/deleteMovieById/{movieId}")
+    @CrossOrigin(origins = "http://localhost:4200")
     public String deleteMovieById(@PathVariable int movieId) {
         return movieService.deleteMovieById(movieId);
 
