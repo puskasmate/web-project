660d71edc676055c277f40ecc78136ba760bf420
Máté Puskás <puskasmate2000@gmail.com>
2020-12-19 14:46:08 +0100

    Inserted a few rows to database.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO movie_table (movie_name, movie_theme, movie_rating) VALUES
+('The Shawshank Redemption', 'Drama', 9.3),
+('The Godfather', 'Crime, Drama', 9.2),
+('The Godfather: Part II', 'Crime, Drama', 9.0),
+('The Dark Knight', 'Action, Crime, Drama', 9.0);
